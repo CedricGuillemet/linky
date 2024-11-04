3874fcd825a98d80a04d57505843e6294ac9d34a
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2024-11-04 21:52:29 +0100

    Update xia.py

--- xia.py
+++ xia.py
@@ -1,34 +1,25 @@
-import pygatt
-from binascii import hexlify
+from atc_mi_interface import AtcMiInterface
 
-# Replace with the MAC address of your BLE device
-DEVICE_ADDRESS = "A4:C1:38:2E:0F:6E"
-HANDLE = 0x0038  # The handle to which we are writing
-VALUE = "0100"  # The value to write, in hexadecimal
+# Initialize the interface (specify the appropriate connection parameters)
+interface = AtcMiInterface()
 
-def notification_handler(handle, value):
-    # This function will be called when data is received from the device
-    print(f"Notification from handle {handle}: {hexlify(value)}")
+# Connect to the interface
+interface.connect()
 
-# Initialize the Bluetooth adapter (e.g., for Linux systems)
-adapter = pygatt.GATTToolBackend()
+# Define the MAC address
+mac_address = "A4:C1:38:2E:0F:6E"
 
-try:
-    # Start the adapter and connect to the device
-    adapter.start()
-    device = adapter.connect(DEVICE_ADDRESS)
+# Function to get information from the MAC address
+def get_info_from_mac(mac):
+    # Assuming there's a method to get info by MAC address
+    info = interface.get_device_info(mac)
+    return info
 
-    # Write the specified value to the handle to enable notifications
-    device.char_write_handle(HANDLE, bytes.fromhex(VALUE), wait_for_response=True)
-    
-    # Subscribe to the handle to listen for notifications
-    print("Listening for notifications...")
-    device.subscribe(HANDLE, callback=notification_handler)
+# Get the information for the specified MAC address
+device_info = get_info_from_mac(mac_address)
 
-    # Keep the script running to continuously receive notifications
-    input("Press Enter to exit...\n")
+# Print the device information
+print(device_info)
 
-finally:
-    # Disconnect and stop the adapter
-    device.disconnect()
-    adapter.stop()
+# Don't forget to disconnect after the operation
+interface.disconnect()
